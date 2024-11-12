0c125d827ee3343634f54fbc290567e29a4e3fd6
xavijacome02 <xavijacome02@outlook.es>
2024-11-12 10:32:59 -0500

    Se crea el metodo recibir la cual confirma cuantas ha recibido del pedido

--- inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidosBDD.java
+++ inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidosBDD.java
@@ -15,55 +15,106 @@ import com.krakedev.inventarios.excepciones.KrakedevException;
 import com.krakedev.inventarios.utils.ConexionBDD;
 
 public class PedidosBDD {
-	public void insertar(Pedidos pedido) throws KrakedevException{
+	public void insertar(Pedidos pedido) throws KrakedevException {
 		Connection con = null;
 		PreparedStatement ps = null;
-		PreparedStatement psDet=null;
-		//FECHA DEL SISTEMA
+		PreparedStatement psDet = null;
+		// FECHA DEL SISTEMA
 		Date fechaActual = new Date();
-		java.sql.Date fechaSQL= new java.sql.Date(fechaActual.getTime());
-		ResultSet rsClave=null;
-		int codigoCabecera=0;
+		java.sql.Date fechaSQL = new java.sql.Date(fechaActual.getTime());
+		ResultSet rsClave = null;
+		int codigoCabecera = 0;
 		try {
-			con=ConexionBDD.obtenerConexion();//ESA CONSTANTE SIRVE PARA QUE RETORNE LAS CLAVES GENERADS
-			ps=con.prepareStatement("insert into cabecera_pedido(proveedor,fecha,estado)values(?,?,?)",Statement.RETURN_GENERATED_KEYS);
+			con = ConexionBDD.obtenerConexion();// ESA CONSTANTE SIRVE PARA QUE RETORNE LAS CLAVES GENERADS
+			ps = con.prepareStatement("insert into cabecera_pedido(proveedor,fecha,estado)values(?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
 			ps.setString(1, pedido.getProveedor().getIdentificador());
 			ps.setDate(2, fechaSQL);
-			ps.setString(3, "S"); //SE CREA Y SE PONE DE ESTADO SOLICITADO PORQUE RECIEN ESTA CREADO
-			
+			ps.setString(3, "S"); // SE CREA Y SE PONE DE ESTADO SOLICITADO PORQUE RECIEN ESTA CREADO
+
 			ps.executeUpdate();
-			
-			rsClave= ps.getGeneratedKeys();
-			
-			if(rsClave.next()) {
-				codigoCabecera= rsClave.getInt(1);
+
+			rsClave = ps.getGeneratedKeys();
+
+			if (rsClave.next()) {
+				codigoCabecera = rsClave.getInt(1);
 			}
-			ArrayList<DetallePedido> detallesPedidos=pedido.getDetalles();
+			ArrayList<DetallePedido> detallesPedidos = pedido.getDetalles();
 			DetallePedido det;
-			for(int i=0;i<detallesPedidos.size();i++) {
-				det=detallesPedidos.get(i);
-				psDet=con.prepareStatement("insert into detalle_pedido(cabecera_pedido,producto,cantidad_solicitada,subtotal,cantidad_recibida) "
-						+ "values(?,?,?,?,?)");
+			for (int i = 0; i < detallesPedidos.size(); i++) {
+				det = detallesPedidos.get(i);
+				psDet = con.prepareStatement(
+						"insert into detalle_pedido(cabecera_pedido,producto,cantidad_solicitada,subtotal,cantidad_recibida) "
+								+ "values(?,?,?,?,?)");
 				psDet.setInt(1, codigoCabecera);
 				psDet.setInt(2, det.getProducto().getCodigo());
 				psDet.setInt(3, det.getCantidadSolicidad());
-				BigDecimal pv= det.getProducto().getPrecioVenta();
-				BigDecimal cantidad=new BigDecimal(det.getCantidadSolicidad());
-				BigDecimal subTotal=pv.multiply(cantidad);
+				BigDecimal pv = det.getProducto().getPrecioVenta();
+				BigDecimal cantidad = new BigDecimal(det.getCantidadSolicidad());
+				BigDecimal subTotal = pv.multiply(cantidad);
 				psDet.setBigDecimal(4, subTotal);
 				psDet.setInt(5, 0);
-				
+
 				psDet.executeUpdate();
-				
+
 			}
-			System.out.println("codigo generado---> "+codigoCabecera);
-			
+			System.out.println("codigo generado---> " + codigoCabecera);
+
 		} catch (KrakedevException e) {
 			e.printStackTrace();
 			throw e;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new KrakedevException("Error al insertar un pedido. Detalle: "+e.getMessage());
+			throw new KrakedevException("Error al insertar un pedido. Detalle: " + e.getMessage());
 		}
+	}
+
+	public void recibir(Pedidos pedido) throws KrakedevException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		PreparedStatement psDet = null;
+		DetallePedido det = null;
+		ResultSet rs = null;
+
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement("update cabecera_pedido set estado=? where numero=?");
+			ps.setString(1, "R");
+			ps.setInt(2, pedido.getCodigo());
+			ps.executeUpdate();
+			ArrayList<DetallePedido> detallesPedidos = pedido.getDetalles();
+
+			for (int i = 0; i < detallesPedidos.size(); i++) {
+				det = detallesPedidos.get(i);
+
+				psDet = con.prepareStatement("select cantidad_solicitada from detalle_pedido where codigo=?");
+				psDet.setInt(1, pedido.getCodigo());
+				rs = psDet.executeQuery();
+				BigDecimal cantidadSolicitada=null;
+				if (rs.next()) {
+					 cantidadSolicitada = rs.getBigDecimal("cantidad_solicitada");
+				}
+				
+				psDet = con.prepareStatement(
+						"update detalle_pedido set cantidad_recibida=?, subtotal=? " + "where codigo=?");
+				psDet.setInt(1, det.getCantidadRecibida());
+				
+				BigDecimal pv = det.getProducto().getPrecioVenta();
+				BigDecimal subTotal = pv.multiply(cantidadSolicitada);
+				psDet.setBigDecimal(2, subTotal);
+				psDet.setInt(3, det.getCodigo());
+				psDet.executeUpdate();
+				System.out.println("cantidad recibe" + pv);
+				System.out.println("cantidad solicitada" + cantidadSolicitada);
+				System.out.println("subtotal" + subTotal);
+			}
+
+		} catch (KrakedevException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			throw new KrakedevException("Error al actualizar el estado del pedido. Detalle: " + e.getMessage());
+		}
+
 	}
 }
--- inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosPedidos.java
+++ inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosPedidos.java
@@ -2,7 +2,9 @@ package com.krakedev.inventarios.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.krakedev.inventarios.bdd.PedidosBDD;
@@ -21,6 +23,21 @@ public class ServiciosPedidos {
 		} catch (KrakedevException e) {
 			e.printStackTrace();
 			return Response.serverError().build();
+		}
+	}
+	@Path("recibir")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	
+	public Response recibir(Pedidos pedido) {
+		PedidosBDD pedidoBDD= new PedidosBDD();
+		try {
+			pedidoBDD.recibir(pedido);
+			return Response.ok().build();
+		} catch (KrakedevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+
 		}
 	}
 }
